6596bc2e02702c788f1ddef1788493bc12b9646b
Syed Aman <sna274@nyu.edu>
2016-11-30 08:44:15 -0500

    create login page

---
+++ login.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+<title>Login with Users Online Tutorial</title>
+<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+<form action="login.php" method="post">
+<div id="border">
+<table cellpadding="2" cellspacing="0" border="0">
+<tr>
+<td>Username:</td>
+<td><input type="text" name="username" /></td>
+</tr>
+<tr>
+<td>Password:</td>
+<td><input type="password" name="password" /></td>
+</tr>
+<tr>
+<td colspan="2" align="center"><input type="submit" name="submit" value="Login" /></td>
+</tr>
+<tr>
+<td align="center" colspan="2"><a href="register.php">Register</a> | <a href="forgot.php">Forgot Pass</a></td>
+</tr>
+</table>
+</div>
+</form>
+</body>
+</html>
